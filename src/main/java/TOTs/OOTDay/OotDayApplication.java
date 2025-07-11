c34a4fa75e2ad14d55a4c900c7c3b003b437a0b5
taek <wontaek88@gmail.com>
2025-07-11 21:55:54 +0900

    Feat: Spring Project 생성

---
+++ src/main/java/TOTs/OOTDay/OotDayApplication.java
@@ -0,0 +1,13 @@
+package TOTs.OOTDay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OotDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OotDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TOTs/OOTDay/OotDayApplicationTests.java
@@ -0,0 +1,13 @@
+package TOTs.OOTDay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OotDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
