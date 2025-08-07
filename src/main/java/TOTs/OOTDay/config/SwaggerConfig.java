c5153d431faf61570b25ca86be65b194c847b339
taek <wontaek88@gmail.com>
2025-08-07 17:39:25 +0900

    [OOTDay-46] 스웨거 설정 추가

---
+++ src/main/java/TOTs/OOTDay/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package TOTs.OOTDay.config;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme()))
+                .security(Collections.singletonList(new SecurityRequirement().addList("bearerAuth")))
+                .info(new Info()
+                        .title("OOTDay API")
+                        .description("4th-MVP-OOTDay-Server의 API 문서입니다.")
+                        .version("1.0.0"));
+    }
+
+    private SecurityScheme securityScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+    }
+}
