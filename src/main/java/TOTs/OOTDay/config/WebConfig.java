d1b879c7f2fa9d72ff9be13117932d99a9bfa640
송성호 <thdtjdgh1679@naver.com>
2025-08-18 16:41:35 +0900

    [OOTDay-57] 로그인 및 Swagger 추가

--- src/main/java/TOTs/OOTDay/config/WebConfig.java
+++ src/main/java/TOTs/OOTDay/config/WebConfig.java
@@ -18,6 +18,12 @@ public class WebConfig implements WebMvcConfigurer {
                         "/api/members/refresh", // Access Token 재발급
                         "/api/members/logout", // 로그아웃
                         "/sms/**", // sms 관련 모든 것
+                        //Swagger 관련
+                        "/swagger-ui.html",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**",
+                        "/swagger-resources/**",
+                        "/webjars/**",
                         "/error"
                 );
     }
