d5aaf413a9fedf724ccb1473ee85282f6fc9e8c9
taek <wontaek88@gmail.com>
2025-08-20 22:17:47 +0900

    [OOTDay] mood, place에 기본생성자 추가

--- src/main/java/TOTs/OOTDay/styling/stylingoption/domain/MoodDto.java
+++ src/main/java/TOTs/OOTDay/styling/stylingoption/domain/MoodDto.java
@@ -2,8 +2,10 @@ package TOTs.OOTDay.styling.stylingoption.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class MoodDto {
 
--- src/main/java/TOTs/OOTDay/styling/stylingoption/domain/PlaceDto.java
+++ src/main/java/TOTs/OOTDay/styling/stylingoption/domain/PlaceDto.java
@@ -2,9 +2,11 @@ package TOTs.OOTDay.styling.stylingoption.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class PlaceDto {
 
     private String placeName;
