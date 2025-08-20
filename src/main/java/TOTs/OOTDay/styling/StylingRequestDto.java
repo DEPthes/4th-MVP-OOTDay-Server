4ccc332935ddb964c2dbfc312f95cd447ac4e549
taek <wontaek88@gmail.com>
2025-08-20 22:12:23 +0900

    [OOTDay] request dto에 기본 생성자 추가

--- src/main/java/TOTs/OOTDay/styling/StylingRequestDto.java
+++ src/main/java/TOTs/OOTDay/styling/StylingRequestDto.java
@@ -3,11 +3,14 @@ package TOTs.OOTDay.styling;
 import TOTs.OOTDay.styling.stylingoption.domain.MoodDto;
 import TOTs.OOTDay.styling.stylingoption.domain.PlaceDto;
 import TOTs.OOTDay.wardrobe.ClothingRequest;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 public class StylingRequestDto {
 
     private List<ClothingRequest> imageList;
